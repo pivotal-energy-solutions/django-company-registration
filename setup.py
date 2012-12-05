b918c19989552a3ee84e8113159542adaabf9017
Steven Klass <sklass@pointcircle.com>
2012-12-05 14:35:12 -0700

    ......

--- setup.py
+++ setup.py
@@ -1,42 +1,7 @@
 # -*- coding: utf-8 -*-
 """setup.py: Django django-company-registration"""
 
-from distutils.core import setup
-import os
-
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-_dir = 'company_registration'
-
-for dirpath, dirnames, filenames in os.walk(_dir):
-    # Ignore PEP 3147 cache dirs and those whose names start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.') or dirname == '__pycache__':
-            del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
+from setuptools import find_packages, setup
 
 setup(name='django-company-registration',
       version='1.0',
@@ -56,7 +21,8 @@ setup(name='django-company-registration',
            'Programming Language :: Python',
            'Topic :: Software Development',
       ],
-      packages=packages,
-      data_files=data_files,
+      packages=find_packages(exclude=['tests', 'tests.*']),
+      package_data={'company_registration': ['static/js/*.js', 'templates/registration/*.html']},
+      include_package_data=True,
       requires=['django (>=1.2)',],
 )
