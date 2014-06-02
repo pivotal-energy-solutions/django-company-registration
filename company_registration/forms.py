e42cb3bf30b48313704fb262d3c2df0084d08f3e
Michael Jeffrey <michaeldjeffrey@gmail.com>
2014-06-02 12:17:24 -0700

    Don't check for other users with the same email.

--- company_registration/forms.py
+++ company_registration/forms.py
@@ -80,16 +80,6 @@ class CompanyRegistrationForm(forms.ModelForm):
             fields += ('photo',)
         exclude= ('user', 'username', 'alt_companies', 'is_active')
 
-    def clean_email(self):
-        """Validate that the email address is not already in use."""
-        User = get_user_model()
-        try:
-            if User.objects.filter(email__iexact=self.cleaned_data['email']).count() >= 1:
-                raise forms.ValidationError(_("A user with that email address is not available"))
-        except User.DoesNotExist:
-            pass
-        return self.cleaned_data['email']
-
 
 class SetPasswordFormTOS(SetPasswordForm):
     """
