26e206ac7076d2d2d6d85a58eb35f7275f188dcf
Steven Klass <sklass@pointcircle.com>
2017-01-16 15:16:25 -0700

    Stop - Change gears

--- company_registration/__init__.py
+++ company_registration/__init__.py
@@ -9,13 +9,13 @@ __copyright__ = 'Copyright 2012-2013 Pivotal Energy Solutions. All rights reserv
 __credits__ = ['Steven Klass', ]
 __license__ = 'See the file LICENSE.txt for licensing information.'
 
-from django.contrib.sites import models as site_models
 
 def get_version():
     return __version__
 
 
 def get_site(request):
+    from django.contrib.sites import models as site_models
     request_site = site_models.RequestSite(request)
     if site_models.Site._meta.installed:
         try:
