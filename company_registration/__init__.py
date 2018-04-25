34e1f358f0581b4f9c902f000976205530dcc252
Steven Klass <rh0dium@users.noreply.github.com>
2018-04-25 15:46:14 -0600

    Lots of updates so we can acutally package this thing

--- company_registration/__init__.py
+++ company_registration/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 """__init__.py: Django company_registration package container"""
 
-__author__ = 'Steven Klass'
-__version_info__ = (0, 1, 0)
+__name__ = 'company_registration'
+__author__ = 'Pivotal Energy Solutions'
+__version_info__ = (1, 0, "0rc1")
 __version__ = '.'.join(map(str, __version_info__))
 __date__ = '3/28/12 12:55 PM'
 __copyright__ = 'Copyright 2012-2013 Pivotal Energy Solutions. All rights reserved.'
